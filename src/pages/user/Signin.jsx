d783d28c2f69138298b0bd47a0a2263007c08cc1
Muhammed Shereef <126606666+muhammedshereefn@users.noreply.github.com>
2024-08-22 11:35:50 +0530

    Update Signin.jsx

--- src/pages/user/Signin.jsx
+++ src/pages/user/Signin.jsx
@@ -101,7 +101,7 @@ const Signin = () => {
                         name="email"
                         type="email"
                         autoComplete="email"
-                        placeholder="abc@gmail.com"
+                        placeholder="email : test@gmail.com"
                         className="w-full placeholder:text-sm text-white bg-[#ffffff0f] border-1 border-slate-500 px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:border-blue-500"
                       />
                       <ErrorMessage
@@ -124,7 +124,7 @@ const Signin = () => {
                         name="password"
                         type="password"
                         autoComplete="current-password"
-                        placeholder="Password"
+                        placeholder="Password : 111111"
                         className="w-full placeholder:text-sm text-white bg-[#ffffff0f] border-1 border-slate-500 px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:border-blue-500"
                       />
                       <ErrorMessage
